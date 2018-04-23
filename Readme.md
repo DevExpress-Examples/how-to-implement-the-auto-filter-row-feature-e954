50b6285bada60f0875120b5e13f0b8d70564062a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to implement the Auto Filter Row feature
+
+
+<p>The following example demonstrates how enable the <strong>Auto Filter Row</strong> feature.</p>
+
+<br/>
+
+
---
+++ VB/Window1.xaml.vb
@@ -0,0 +1,47 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Collections.Generic
+Imports DevExpress.Xpf.Grid
+Imports DevExpress.Data.Filtering
+
+Namespace AutoFilterRow
+
+	Partial Public Class Window1
+		Inherits Window
+		Private list As List(Of TestData)
+
+		Public Sub New()
+			InitializeComponent()
+
+			list = New List(Of TestData)()
+			For i As Integer = 0 To 999
+				list.Add(New TestData() With {.Text = Guid.NewGuid().ToString(), .Number = i})
+			Next i
+			grid.ItemsSource = list
+		End Sub
+	End Class
+
+	Public Class TestData
+		Private privateText As String
+		Public Property Text() As String
+			Get
+				Return privateText
+			End Get
+			Set(ByVal value As String)
+				privateText = value
+			End Set
+		End Property
+		Private privateNumber As Integer
+		Public Property Number() As Integer
+			Get
+				Return privateNumber
+			End Get
+			Set(ByVal value As Integer)
+				privateNumber = value
+			End Set
+		End Property
+	End Class
+
+End Namespace
