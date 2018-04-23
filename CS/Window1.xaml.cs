9ed34942b3a21ca92c4e44dc05f01075e791b40b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using DevExpress.Xpf.Grid;
+using DevExpress.Data.Filtering;
+
+namespace AutoFilterRow {
+
+    public partial class Window1 : Window {
+        List<TestData> list;
+
+        public Window1() {
+            InitializeComponent();
+
+            list = new List<TestData>();
+            for (int i = 0; i < 1000; i++) {
+                list.Add(new TestData() { Text = Guid.NewGuid().ToString(), Number = i });
+            }
+            grid.DataSource = list;
+        }
+    }
+
+    public class TestData {
+        public string Text { get; set; }
+        public int Number { get; set; }
+    }
+
+}
